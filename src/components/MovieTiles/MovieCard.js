31456af9330fe87347954b1b3fa0ffdab2b0fe75
Erin David <erindavid001@gmail.com>
2022-04-28 15:03:42 -0700

    create movie tiles component directory

---
+++ src/components/MovieTiles/MovieCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import link 
+import { Link } from 'react-router-dom';
+
+
+function MovieCard() {
+  return (
+    <article className='movie-card'>
+        <img src={"https://i.pinimg.com/736x/90/22/9a/90229a43d797a636c6768ce4e8b68d5c.jpg"} alt="no poster available"/>
+        <h1>test</h1>
+    </article>
+  )
+}
+
+export default MovieCard

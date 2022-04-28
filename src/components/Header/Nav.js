750c0b28971d62fe47a9e5740a655184155a99ae
Fiona Y <fyeung21@gmail.com>
2022-04-28 14:50:31 -0700

    Change div to nav for more semantic tag

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -2,14 +2,14 @@ import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
-      <div className="nav">
+      <nav className="nav">
           <ul>
               <li><NavLink to="/">home</NavLink></li>
               <li><NavLink to="/favourites">favourites</NavLink></li>
               <li><NavLink to="/about">about</NavLink></li>
               <li><NavLink to="/user">user</NavLink></li>
           </ul>
-      </div>
+      </nav>
     );
   }
   

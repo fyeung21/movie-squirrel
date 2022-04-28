a4175430bad162830886a97bf0381ab937f29231
Erin David <erindavid001@gmail.com>
2022-04-28 15:13:15 -0700

    add placeholder movie tiles

--- src/components/MovieTiles/MovieCard.js
+++ src/components/MovieTiles/MovieCard.js
@@ -6,8 +6,12 @@ import { Link } from 'react-router-dom';
 function MovieCard() {
   return (
     <article className='movie-card'>
-        <img src={"https://i.pinimg.com/736x/90/22/9a/90229a43d797a636c6768ce4e8b68d5c.jpg"} alt="no poster available"/>
-        <h1>test</h1>
+        <img src={"https://www.themoviedb.org/t/p/w300_and_h450_bestv2/eWdyYQreja6JGCzqHWXpWHDrrPo.jpg"} alt="no poster available"/>
+
+        <div>
+            <h2>test</h2>
+            <p><Link to="/">More info</Link></p>
+        </div>
     </article>
   )
 }
---
+++ src/components/MovieTiles/MoviesContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MoviesContainer() {
+  return (
+    <div className='movies-container'>
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+    </div>
+  )
+}
+
+export default MoviesContainer

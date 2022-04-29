b7f90ba5a0cb3452e28c1f9f5716807dbd781b44
Erin David <erindavid001@gmail.com>
2022-04-29 13:51:00 -0700

    add sort navigation feature

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,16 +1,18 @@
 import { useState, useEffect } from "react";
 import { API_KEY } from "../globals/globals";
 import MoviesContainer from "../components/MovieTiles/MoviesContainer";
+import SortNav from "../components/sort-nav/SortNav";
 
-const PageHome = () => {
+const PageHome = ({sort}) => {
 
   // store the 12 tiles here
   const [moviesData, setMoviesData] = useState(false);
 
+
   useEffect(()=> {
     const fetchMovies = async () => {
       // make a php-esque query here
-        const query = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+        const query = await fetch(`https://api.themoviedb.org/3/movie/${sort}?api_key=${API_KEY}&language=en-US&page=1`);
         // res is the result
         const res = await query.json();
         
@@ -18,16 +20,17 @@ const PageHome = () => {
         const firstTwelve = res.results.splice(0, 12);
         setMoviesData(firstTwelve);
 
-        console.log(res);
+        // console.log(res);
     }
     fetchMovies();
 
-}, [])  
+}, [sort])  
     return (
-      <div className="pageHome">
+      <section className="pageHome">
+        <SortNav />
         {/* movies data is where the information from the query will be stored in the prop */}
           {moviesData !== false && <MoviesContainer moviesData={moviesData}/>}
-      </div>
+      </section>
     );
   }
   

4e9cab8f9f2b5ebc58971165094fde9292c6ed4f
Fiona Y <fyeung21@gmail.com>
2022-04-29 12:14:46 -0700

    Apply react helmet to all pages

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 // import { appTitle } from '../globals/globalVariables';
+import { Helmet } from 'react-helmet';
 
 const PageNotFound = () => {
 
@@ -9,11 +10,17 @@ const PageNotFound = () => {
     }, []);
 
     return (
-      <div className="pageNotFound">
+      <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>404 Page Not Found | Movie Squirrel</title>
+        </Helmet>
+        <div className="pageNotFound">
           <h1>404 page</h1>
           <p>Page not found.</p>
 			  	<p>Go to <Link to="/">Home</Link> page.</p>
-      </div>
+        </div>
+      </>
     );
   }
   

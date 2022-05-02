330df6cf3bf59636ab671ebd8a67c65eadda89d4
Erin David <erindavid001@gmail.com>
2022-05-02 12:42:20 -0700

    connect single movie link to API

--- src/pages/PageMovieSingle.js
+++ src/pages/PageMovieSingle.js
@@ -1,7 +1,35 @@
 import { Helmet } from 'react-helmet';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { API_KEY } from '../globals/globals';
 import SingleMovie from '../components/singleMovie/SingleMovie';
 
 const PageMovieSingle = () => {
+
+  // store single movie data here
+  const [movieData, setMovieData] = useState(false);
+
+  const {id} = useParams();
+
+  // do query here
+
+  useEffect(() => {
+    
+     // do query here
+     const fetchMovieData = async () =>{
+        // store the query here
+        const query = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`);
+        // store result from query here
+        const res = await query.json();
+
+        // store the selected movie into the movie data const
+        setMovieData(res);
+        console.log(res);
+     }
+
+     fetchMovieData();
+  },[])
+  
     return (
       <>
         <Helmet>
@@ -10,7 +38,8 @@ const PageMovieSingle = () => {
         </Helmet>
         <div className="pageMovieSingle">
           <h1>single movie page</h1>
-          <SingleMovie />
+          {/* store the single movie data in a prop */}
+          <SingleMovie movie={movieData}/>
         </div>
       </>
     );

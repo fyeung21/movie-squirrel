968fe13a5291c309e6b0e6cf5f8175b2e4c3b2eb
Fiona Y <fyeung21@gmail.com>
2022-05-02 20:32:11 -0700

    Style mobile nav

--- src/components/Header/MobileNav.js
+++ src/components/Header/MobileNav.js
@@ -5,25 +5,27 @@ import FilmIcon from '../Icons/FilmIcon';
 
 const MobileNav = () => {
   return (
-    <nav className="mobile-nav">
-      <ul>
-        <li>
-          <NavLink to="/">
-            <FilmIcon />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/favourites">
-            <HeartFill />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">
-            <AboutIcon />
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <section className="mobile-nav">
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">
+              <FilmIcon />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/favourites">
+              <HeartFill />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">
+              <AboutIcon />
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
   );
 };
 

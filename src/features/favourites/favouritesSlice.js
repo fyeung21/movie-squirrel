2ccc53a6ca205597ad6e87a0b1b2eca3ec38d965
Fiona Y <fyeung21@gmail.com>
2022-05-19 22:51:26 -0700

    Add removeFave functionality

--- src/features/favourites/favouritesSlice.js
+++ src/features/favourites/favouritesSlice.js
@@ -1,5 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const getIndex = (item, arr) => {
+  // findIndex() in array from when argument is first true
+  // find the index of where the item from the parameter is in the array
+  return arr.findIndex(arrItem => arrItem.id === item.id);
+}
 
 export const favouritesSlice = createSlice({
   name: 'favourites',
@@ -7,15 +12,23 @@ export const favouritesSlice = createSlice({
     value: []
   },
   reducers: {
-    //add functions that will manipulate the state
+    // Add functions that will manipulate the state
     addFavourite: (state, action) => {
         console.log(state.value);
         console.log(action.payload);
         state.value = [...state.value, action.payload];
     },
     removeFavourite: (state, action) => {
-        state.value.pop(action.payload);
-        console.log('removing fav...TO DO!!!')
+        const favesArray = state.value;
+        // splice(`insert at index`, `number of elements to replace`, `value to insert`)
+        // This splice matches the movie index in the favesArray and replaces 1 element with value=blank
+        // When value parameter is not declared, splice removes the targeted element from array
+        // therefore, removes targeted movie object from favesArray
+        console.log(action.payload)
+        console.log(state.value)
+        favesArray.splice(getIndex(action.payload, state.value), 1);
+        state.value = favesArray;
+        console.log('removing fav...')
     }
   }
 })

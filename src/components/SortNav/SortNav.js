0e5532f61fb2415d31da37acda7bbe6aa3f853da
Fiona Yeung <49259721+fyeung21@users.noreply.github.com>
2022-05-21 13:02:11 -0700

    Revert "styling revision"

--- src/components/SortNav/SortNav.js
+++ src/components/SortNav/SortNav.js
@@ -1,25 +1,24 @@
 import { NavLink } from "react-router-dom";
-import React from 'react';
 
+import React from 'react'
 
 const SortNav = () => {
   return (
     <nav>
-        <select>
-            <option>
+        <ul>
+            <li>
                 <NavLink to="/sort/popular">Popular</NavLink>
-            </option>
-            <option>
+            </li>
+            <li>
                 <NavLink to="/sort/top-rated">Top Rated</NavLink>
-            </option>
-            <option>
+            </li>
+            <li>
                 <NavLink to="/sort/upcoming">Upcoming</NavLink>
-            </option>
-            <option>
+            </li>
+            <li>
                 <NavLink to="/sort/now-playing">Now Playing</NavLink>
-            </option>
-        
-        </select>
+            </li>
+        </ul>
     </nav>
   )
 }

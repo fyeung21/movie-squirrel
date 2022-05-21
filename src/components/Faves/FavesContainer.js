b9e3b182e91a6a3502e72ad8f37d52981a1b8212
Aleumkim <101660099+Aleumkim@users.noreply.github.com>
2022-05-20 22:29:55 -0700

    styling revision

--- src/components/Faves/FavesContainer.js
+++ src/components/Faves/FavesContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import MovieCard from '../MovieTiles/MovieCard';
+
 
 const FavesContainer = () => {
     //map out movie cards from faves slice
   
   return (    
       <section className='faves-container'>
-
+ 
       </section>
   )
 }
--- src/pages/PageFaves.js
+++ src/pages/PageFaves.js
@@ -1,8 +1,35 @@
 import { Helmet } from 'react-helmet';
+import { API_KEY } from "../globals/globals";
 import FavesContainer from '../components/Faves/FavesContainer';
 import MovieCard from '../components/MovieTiles/MovieCard';
+import { useState, useEffect } from "react";
 
-const PageFaves = () => {
+const PageFaves = ({sort}) => {
+
+
+
+
+    const [moviesData, setMoviesData] = useState(false);
+
+
+    useEffect(()=> {
+      const fetchMovies = async () => {
+        // make a php-esque query here
+          const query = await fetch(`https://api.themoviedb.org/3/movie/${sort}?api_key=${API_KEY}&language=en-US&page=1`);
+          // res is the result
+          const res = await query.json();
+          
+          // get the first 12 movies here, then set them as the data
+          const firstTwelve = res.results.splice(0, 12);
+          setMoviesData(firstTwelve);
+  
+          // console.log(res);
+      }
+      fetchMovies();
+  
+
+}, [sort])  
+
   return (
     <>
       <Helmet>

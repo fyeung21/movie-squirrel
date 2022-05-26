205077340624214a20c8ec45b11dc1e96b62f735
Erin David <erindavid001@gmail.com>
2022-05-25 19:52:40 -0700

    insert icon component to logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,9 +1,10 @@
 import { NavLink } from 'react-router-dom';
+import SquirrelIcon from '../Icons/SquirrelIcon';
 
 const Logo = ({title}) => {
     return (
       <div className="logo">
-            <h1><NavLink to="/">{title}</NavLink></h1>
+            <h1><NavLink to="/">{title}<SquirrelIcon/></NavLink></h1>
       </div>
     );
   }
